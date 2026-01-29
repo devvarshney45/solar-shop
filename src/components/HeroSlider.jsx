83e8c6814dbd3b92f412ae3261e61030cf93e061
devvarshney45 <varshneydev365@gmail.com>
2026-01-29 03:46:07 -0800

    new images edit

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -4,6 +4,8 @@ import { Autoplay } from "swiper/modules";
 
 export default function HeroSlider() {
   const images = [
+    "https://lumprodblobcdn.azureedge.net/Banners/e16afcfb-fde6-4e35-9b03-dfaac91263f4_21st.jpg?w=1688&h=520&q=60&format=webp",
+    "https://lumprodblobcdn.azureedge.net/Banners/14978cc1-62d0-4c16-b39b-805afb8e3ace_1fc14800-42c1-4ecc-9385-ed5bf76bb233_P1200DesktopBanner_11zon.jpg?w=1688&h=520&q=60&format=webp",
     "/solar1.jpg",
     "/solar2.jpg",
     "/solar3.jpg",
